35efc5d35b485283ad876dbfaf78180bc1728266
GYGGFDUSHSH <92365864+GYGGFDUSHSH@users.noreply.github.com>
2021-10-13 13:04:22 +0530

    Create commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file_name} \n @Sflix2k",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '🎖️Our Group🎖️', url="https://t.me/Sflix2kchats"
                                 )
                         ]
                     ]
